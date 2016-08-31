0a454d58d907ca2174dcdb7a95f6e2c6d300d3ee
Jovin Mwilanga <jovinbeda@hotmail.com>
2016-08-31 14:32:32 +0300

    added ioredis support

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var BPromise = require('bluebird');
-var redis    = require('redis');
-var ajv      = require("ajv")({
+var Redis = require('ioredis');
+var ajv   = require("ajv")({
   removeAdditional: false
 });
-BPromise.promisifyAll(redis.RedisClient.prototype);
-BPromise.promisifyAll(redis.Multi.prototype);
 
 /**
  *
@@ -50,9 +47,9 @@ var RedisWrapper = function (config) {
     throw new Error("config.validateKey is not a function");
   }
   
-  var redis_client;
+  var client;
   
-  redis_client = redis.createClient({
+  client = new Redis({
     host          : config.host,
     port          : config.port,
     db            : config.db_number,
@@ -63,33 +60,33 @@ var RedisWrapper = function (config) {
     }
   });
   
-  redis_client.on("connect", function () {
+  client.on("connect", function () {
     console.info('REDIS CLIENT CONNECTED TO SERVER');
   });
   
-  redis_client.on("ready", function () {
+  client.on("ready", function () {
     console.info("REDIS CLIENT READY:: CONNECTED TO REDIS DB " + config.db_number + " AT ADDRESS " + config.host + ":" + config.port);
   });
   
-  redis_client.on("reconnecting", function () {
+  client.on("reconnecting", function () {
     console.warn('REDIS CLIENT RECONNECTING');
   });
   
-  redis_client.on("error", function (e) {
+  client.on("error", function (e) {
     console.error('REDIS CLIENT ERROR');
     throw e;
   });
   
-  redis_client.on("end", function () {
+  client.on("end", function () {
     console.error('REDIS CLIENT CONNECTION TO REDIS SERVER CLOSED');
   });
   
-  self.name         = 'RedisWrapper';
-  self.host         = config.host;
-  self.port         = config.port;
-  self.db_number    = config.db_number;
-  self.redis_client = redis_client;
-  self.validateKey  = config.validateKey;
+  self.name        = 'RedisWrapper';
+  self.host        = config.host;
+  self.port        = config.port;
+  self.db_number   = config.db_number;
+  self.client      = client;
+  self.validateKey = config.validateKey;
   
   self.bins = {
     hash_keys_expire: 'bin:set_hash_keys_expire'
