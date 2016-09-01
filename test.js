b21084746e81843e925644c28150be4c9df779ae
Jovin Mwilanga <jovinbeda@hotmail.com>
2016-09-02 00:04:26 +0300

    modified hscan

--- test.js
+++ test.js
@@ -8,16 +8,16 @@ var tag_names = ['html_main', 'html_mini', 'main'];
 var ids       = [1, 10, 105];
 var pages     = [1, 6];
 
-function gen(key) {
-  var obj = {
-    key         : key,
-    id          : _.shuffle(ids)[0],
-    key_tag_name: _.shuffle(tag_names)[0],
-    page        : _.shuffle(pages)[0],
-  };
-  var k   = qs.stringify(obj, {delimiter: metadata_separator});
-  return `${start_identifier}${k}${end_identifier}`;
-}
+// function gen(key) {
+//   var obj = {
+//     key         : key,
+//     id          : _.shuffle(ids)[0],
+//     key_tag_name: _.shuffle(tag_names)[0],
+//     page        : _.shuffle(pages)[0],
+//   };
+//   var k   = qs.stringify(obj, {delimiter: metadata_separator});
+//   return `${start_identifier}${k}${end_identifier}`;
+// }
 
 var RedisWrapper = require('./index');
 
@@ -27,15 +27,15 @@ var config = {
   },
   host       : '127.0.0.1',
   port       : 6379,
-  db_number  : 0
+  db_number  : 2
 };
 
 var RedisWrapperInstance = new RedisWrapper(config);
 
-var sample_data = {};
-require('./sample.json').map(function (d) {
-  sample_data[gen(d.last_name)] = d.last_name;
-});
+// var sample_data = {};
+// require('./sample.json').map(function (d) {
+//   sample_data[gen(d.last_name)] = d.last_name;
+// });
 
 //RedisWrapperInstance._hmset({
 //  hash_name           : 'sample',
@@ -46,9 +46,9 @@ require('./sample.json').map(function (d) {
 //});
 
 RedisWrapperInstance._hscan({
-  hash_name           : 'sample',
-  match_pattern       : '$$START_METADATA$$key=Weaver$$sep$$id=*$$sep$$key_tag_name=*$$sep$$page=*$$END_METADATA$$',
-  count               : 100,
+  hash_name           : 'item_cache_bin',
+  match_pattern       : '$$START_METADATA$$key=page_article$$sep$$id=7840$$sep$$page=1$$sep$$version=*$$sep$$key_tag_name=html_main$$END_METADATA$$',
+  count               : 2000,
   should_be_registered: false
 })
   .then(function (d) {
